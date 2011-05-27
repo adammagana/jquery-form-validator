8bfd3f1e473d16d1b3e3f6bec958b489f5d0da95
Adam Magana <adammagana@gmail.com>
2011-05-27 19:01:36 -0400

    Rearranged the basic structure of the plugin to make it more extensible/mogular. Also adjusted the demo file.

--- easy-form-validator.js
+++ easy-form-validator.js
@@ -6,75 +6,114 @@
  * @see http://docs.jquery.com/Plugins/Authoring
  */
 (function($){
-    $.fn.validate = function() {
-        var errors = [];
-        var text_inputs = this.find('input[data-validate][type="text"]'); //selects all text fields with a 'data-validate' attr
+    
+    $.fn.validate = function(options) {
         
-        if(text_inputs.length >= 1) {
-            for(var i = 0;i < text_inputs.length;i++) {
-                var input = $(text_inputs[i]);
-                var input_value = input.val();
-                var validate = input.data('validate');
+        $.fn.validate.defaults = {
+            //default settings go here
+        };
+
+        var settings = $.extend({}, $.fn.validate.defaults, options);
+        
+        return this.each(function(){
+            $(this).find('*[data-validate]').each(function() {
+                var validation = $(this).data('validate').split(/\s/);
                 
-                var validation = [];
-                validation = validate.split(' ');
-                
-                for(var j = 0;j < validation.length;j++) {
-                    switch(validation[j]) {
-                        case 'required':
-                            //make sure the the value is not null
-                            if(input_value === '') {
-                                errors.push('This field is required.');
-                                input.css({
-                                    'background-color':'#f26c4f'
-                                });
-                            }
-                            break;
-                        case 'alpha':
-                            //check that the value is only alphanumeric characters
-                            break;
-                        case 'numeric':
-                            //check that the value is only numeric characters
-                            if(isNaN(parseInt(input_value, 10))) {
-                                errors.push('Not a valid number.');
-                                input.css({
-                                    'background-color':'#f26c4f'
-                                });
-                            }
-                            break;
-                        case 'email':
-                            //do email validation
-                            var regexp = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
-                            if(regexp.test(input_value)) {
-                                //passed the email validation
-                            }else {
-                                errors.push('Email is not valid.');
-                                input.css({
-                                    'background-color':'#f26c4f'
-                                });
-                            }
-                            break;
-                        case 'url':
-                            //do url validation
-                            var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-                            if(regexp.test(input_value)) {
-                                //passed the email validation
-                            }else {
-                                errors.push('URL is not valid.');
-                                input.css({
-                                    'background-color':'#f26c4f'
-                                });
-                            }
-                            break;
-                        default:
-                            //console.log(input, 'Validation type not recognized!');
-                            break;
+                for(var i=0, j=validation.length; i<j; i++) {
+                    var current = validation[i];
+                    if(current in $.fn.validate.validationHooks && typeof $.fn.validate.validationHooks[current] === "function") {
+                        if(true === $.fn.validate.validationHooks[current].apply(this, [$(this).val()])) {
+                            console.log($(this).attr('name')+' validated!');
+                        }else {
+                            console.log($(this).attr('name')+' did not validate!');
+                        }
                     }
                 }
+            });
+        });
+    };
+    
+    $.fn.validate.validationHooks = {
+        required:function(val) {
+            return true;
+        },
+        alpha:function(val) {
+            return true;
+        },
+        numeric:function(val) {
+            return true;
+        },
+        email:function(val) {
+            var regexp = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+            if(regexp.test(val)) {
+                return true;
+            }else {
+                return false;
+            }
+        },
+        url:function(val) {
+            var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+            if(regexp.test(val)) {
+                return true;
+            }else {
+                return false;
             }
         }
-        
-        //If the errors array is empty return true, else return false
-        return (errors.length === 0) ? true : false;
     };
 })(jQuery);
+
+
+/*
+for(var j = 0;j < validation.length;j++) {
+    switch(validation[j]) {
+        case 'required':
+            //make sure the the value is not null
+            if(input_value === '') {
+                errors.push('This field is required.');
+                input.css({
+                    'background-color':'#f26c4f'
+                });
+            }
+            break;
+        case 'alpha':
+            //check that the value is only alphanumeric characters
+            break;
+        case 'numeric':
+            //check that the value is only numeric characters
+            if(isNaN(parseInt(input_value, 10))) {
+                errors.push('Not a valid number.');
+                input.css({
+                    'background-color':'#f26c4f'
+                });
+            }
+            break;
+        case 'email':
+            //do email validation
+            var regexp = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+            if(regexp.test(input_value)) {
+                //passed the email validation
+            }else {
+                errors.push('Email is not valid.');
+                input.css({
+                    'background-color':'#f26c4f'
+                });
+            }
+            break;
+        case 'url':
+            //do url validation
+            var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+            if(regexp.test(input_value)) {
+                //passed the email validation
+            }else {
+                errors.push('URL is not valid.');
+                input.css({
+                    'background-color':'#f26c4f'
+                });
+            }
+            break;
+        default:
+            //console.log(input, 'Validation type not recognized!');
+            break;
+    }
+}
+*/
